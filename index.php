bed712f7640d9d0be5ccc82b69e01f335f054cb5
Syow <31361791+Syowo@users.noreply.github.com>
2018-09-14 21:11:43 +0800

    9/14-21:20
    玩了一下CSS

--- index.php
+++ index.php
@@ -1,36 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh" style="background:gray">
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>智慧型監視器</title>
+
 </head>
 <body>
-  <?php
-      //開頭文字
-      echo "<b>智慧監視器紀錄</b>"."<br><br><br><br>";
-
-      //連接mysql
-      $mysqli = new mysqli('localhost','root','','test1');
-
-      //連接是否失敗 true 錯誤訊息
-      if($mysqli->connect_error)
-        die('連結錯誤訊息:'.$mysqli->connect_error."<br>");
-
-      //sql查詢語法
-      $sql = "select date,hour,min,sec,who from test";
-      $mysqli->query('set names utf8');
-      $sql2=$mysqli->query($sql);
-      //印出資料
-      while ($list3=$sql2->fetch_object()) {
-        // code...
-        echo $list3->date."_".$list3->hour."_";
-        echo $list3->min."_".$list3->sec."---";
-        echo $list3->who."<br>";
-      }
-      $sql2->close();
-      $mysqli->close();
-   ?>
+  <link rel = "stylesheet"  href = "dev.css">
+  <dev><b>智慧監視器紀錄網站</b></dev>
+  <br><img src = "cats.jpg"   > <br>
+  <?php include('database.php'); ?>
+  <!---表單-->
+  <form name="form1" action="inex.php" method="post">
+      <p id="usernamestyle" > 使用者名稱: <input type="text" name="username" maxlength="12"></p>
+      <p id="userpassword"> 密碼: <input type="text" name="password" maxlength="12" ></p>
+       <br> <input id="submitpos" type="submit" value="登入">
+  </form>
 </body>
 </html>
