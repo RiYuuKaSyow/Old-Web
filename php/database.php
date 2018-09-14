556d7802d59b44d8431a06d68799c43163406d94
Syow <31361791+Syowo@users.noreply.github.com>
2018-09-15 01:09:05 +0800

    9/14
    分開資料夾
    嘗試用bootstrap
    php引用檔案相對路徑  ../上一層

--- php/database.php
+++ php/database.php
@@ -1,24 +1,32 @@
 
   <?php
-      //開頭文字
-      echo "<b>智慧監視器紀錄</b>"."<br>" ;
 
+
+      //帳號密碼變數 直接用post會有風險存在
+      $account = $_POST['username'] ;
+      $password = $_POST['password'] ;
       //連接mysql
-      $mysqli = new mysqli('localhost','root','','test1') ;
+      $mysqli = new mysqli('localhost',$account,$password,'test1') ;
 
       //連接是否失敗 true 錯誤訊息
-      if($mysqli->connect_error)
-        die('連結錯誤訊息:'.$mysqli->connect_error."<br>") ;
+      if($mysqli->connect_error){
 
+        die('登入失敗 <br> 連結錯誤訊息:'.$mysqli->connect_error."<br>") ;
+
+      }
       //sql查詢語法
       $sql = "select date,hour,min,sec,who from test" ;
       $mysqli->query('set names utf8') ;
       $sql2 = $mysqli->query($sql) ;
+
+      //開頭文字
+      echo "<b>智慧監視器紀錄</b>"."<br>" ;
+
       //印出資料
       while ($list3 = $sql2->fetch_object()) {
         // code...
         echo $list3->date . "日" . $list3->hour . "點" ;
-        echo $list3->min . "分" . $list3->sec . "秒 : --" ;
+        echo $list3->min . "分" . $list3->sec . "秒   :  " ;
         echo $list3->who . "<br>" ;
       }
       $sql2->close() ;
--- php/record.php
+++ php/record.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="zh" style="background:gray">
+<html lang="en">
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -8,27 +8,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>智慧型監視器</title>
-
+  <title>Document</title>
 </head>
 <body class="bg-info">
-
-  <dev class="form">
-    <label class="text-center font-weight-bold display-3 col-sm-12">
-      智慧監視器
-    </label>
-  </dev>
-  <!---表單-->
-  <form name="form1" action="html/login.html" method="post">
-
-    <button type="submit" class="btn btn-success rounded mx-auto d-block"> 登入 </button>
-
-  </form>
-
-  <br>
-  <img src = "cats.jpg"  class="rounded mx-auto d-block" >
-  <br>
-
-
+  <?php
+    include('database.php');
+  ?>
 </body>
 </html>
