0a26016f244a859d0887021336691d4a12da4686
Syow <crescentsyou@gmail.com>
2018-09-17 22:31:19 +0800

    9/17
    success
    試著用按鈕改變圖片
    以及點擊圖片改變圖片
    --------------------------
    fail
    試著點擊按鈕打開圖片

--- php/database.php
+++ php/database.php
@@ -1,7 +1,4 @@
-
   <?php
-
-
       //帳號密碼變數 直接用post會有風險存在
       $account = $_POST['username'] ;
       $password = $_POST['password'] ;
@@ -15,7 +12,7 @@
 
       }
       //sql查詢語法
-      $sql = "select date,hour,min,sec,who from test" ;
+      $sql = "select id,date,hour,min,sec,who from test" ;
       $mysqli->query('set names utf8') ;
       $sql2 = $mysqli->query($sql) ;
 
@@ -23,11 +20,15 @@
       echo "<b>智慧監視器紀錄</b>"."<br>" ;
 
       //印出資料
-      while ($list3 = $sql2->fetch_object()) {
+      $i=0;
+      while ($list[$i] = $sql2->fetch_object()) {
         // code...
-        echo $list3->date . "日" . $list3->hour . "點" ;
-        echo $list3->min . "分" . $list3->sec . "秒   :  " ;
-        echo $list3->who . "<br>" ;
+        echo $list[$i]->id . '.      ' ;
+        echo $list[$i]->date . "日" . $list[$i]->hour . "點" ;
+        echo $list[$i]->min . "分" . $list[$i]->sec . "秒   :  " ;
+        echo $list[$i]->who .'    ' ;
+        echo '<img id = "img"+i src="../img/cats.jpg" style="display:none" >' . '<input type="button" onclick="imgchange(img234)" value="圖片" >' . '<br>' ;
+        $i++;
       }
       $sql2->close() ;
       $mysqli->close() ;
