43c3de0b601ddbd2354e44a876e4799e60be1030
Syow <crescentsyou@gmail.com>
2018-11-24 21:44:44 +0800

    11/24....

--- php/test.php
+++ php/test.php
@@ -2,11 +2,16 @@
 $m = new MongoDB\Driver\Manager();
 $j = new MongoDB\Driver\Query(['acc' =>'Syow']);
 $i = $m->executeQuery('test.col' , $j ) ;
-foreach ($i as $document) {
-    print_r($document);
+$data = [] ;
 
+foreach ($i as $document) {
+
+    echo $document->mail;
+
+    $data[] = $document ;
 echo '<br>';
 }
+echo $data[0]->acc;
 
 
 ?>
