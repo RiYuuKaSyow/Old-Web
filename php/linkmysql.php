15fd572cd599624338614d5083cf165fbb9c11d4
Syow <crescentsyou@gmail.com>
2018-12-01 00:03:04 +0800

    改密碼

--- php/linkmysql.php
+++ php/linkmysql.php
@@ -3,8 +3,8 @@
   //連接mysql
   //$mysqli = new mysqli('120.101.8.38' , 'root' , '' ,'record') ;
   //$mysqliuser = new mysqli('120.101.8.38' , 'root' , '' ,'user') ;
-  $mysqli = new mysqli('localhost' , 'root' , '' ,'record') ;
-  $mysqliuser = new mysqli('localhost','root','','user') ;
+  $mysqli = new mysqli('localhost' , 'root' , 'admin' ,'record') ;
+  $mysqliuser = new mysqli('localhost','root','admin','user') ;
   $mysqli->query('set names utf8') ;
   $mysqliuser->query('set names utf8') ;
   //連接是否失敗 true 錯誤訊息
