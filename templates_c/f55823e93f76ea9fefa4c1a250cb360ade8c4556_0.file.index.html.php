a61d9ae3cecfcec23d9007f1f70e266b0e99fe0c
Syow <crescentsyou@gmail.com>
2018-12-01 22:47:01 +0800

    12/1

--- templates_c/f55823e93f76ea9fefa4c1a250cb360ade8c4556_0.file.index.html.php
+++ templates_c/f55823e93f76ea9fefa4c1a250cb360ade8c4556_0.file.index.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-12-01 13:28:15
+/* Smarty version 3.1.32, created on 2018-12-01 15:44:05
   from 'E:\xampp\htdocs\html\index.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5c027e5f699d07_58148322',
+  'unifunc' => 'content_5c029e358abd96_10107079',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f55823e93f76ea9fefa4c1a250cb360ade8c4556' => 
     array (
       0 => 'E:\\xampp\\htdocs\\html\\index.html',
-      1 => 1543667292,
+      1 => 1543675443,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5c027e5f699d07_58148322 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5c029e358abd96_10107079 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,42 +53,45 @@ function content_5c027e5f699d07_58148322 (Smarty_Internal_Template $_smarty_tpl)
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>智慧監視器</title>
 
-  <?php echo '<script'; ?>
- type="text/javascript">
-    $(function(){
-      $(document).keydown(function(event){
-        switch (event.keyCode) {
-          //上
-          case 38:
-            $("#img1").animate( { top : '-=20px' } ).delay(100) ;
-            break ;
-          //下
-          case 40:
-            $("#img1").animate( { top : '+=20px' } ).delay(100) ;
-            break ;
-          //左
-          case 39:
-            $("#img1").animate( { left : '+=20px' } ).delay(100) ;;
-            break ;
-          //右
-          case 37:
-            $("#img1").animate( { left : '-=20px' } ).delay(100) ;
-            break ;
-          default:
-            break;
-        }
-      });
-    });
-  <?php echo '</script'; ?>
->
-
 </head>
 <body>
   <!--首頁標題--->
   <div class="text-center col-xs-12 col-sm-12 h1 " >
     首  頁
   </div>
-  <img id="img1" src="../i/img1.jpg" alt="" style=" position: relative; ">
+  <!--首頁內容--->
+  <div class=" container ">
+    <!--圖片--->
+    <div class="row">
+      <div class="col">
+      </div>
+      <div class="col-8">
+        <div id="carousel" class=" carousel slide " data-ride="carousel" data-interval="3500">
+          <div class="carousel-inner text-center">
+            <div class="carousel-item active">
+              <img class="w-50" src="../i/img1.jpg" alt="" style="width:1000px;" >
+            </div>
+            <div class="carousel-item">
+              <img class="w-50" src="../i/img2.jpg" alt="">
+            </div>
+          </div>
+          <a href="#carousel" class="carousel-control-prev" role="button" data-slide="prev" >
+            <span class="bg-dark carousel-control-prev-icon"></span>
+          </a>
+          <a href="#carousel" class="carousel-control-next" role="button" data-slide="next" >
+            <span class="bg-dark carousel-control-next-icon"></span>
+          </a>
+          <ol class=" carousel-indicators" >
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1" ></li>
+          </ol>
+        </div>
+      </div>
+      <div class="col">
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
 <?php }
